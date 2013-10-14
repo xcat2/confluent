8d21674ced97124bed8af6d58c766ceb62bdba8a
Jarrod Johnson <jbjohnso@us.ibm.com>
2013-10-14 09:21:55 -0400

    Implement unix domain socket in socket api

--- confluent/sockapi.py
+++ confluent/sockapi.py
@@ -10,22 +10,26 @@ import confluent.config as config
 import eventlet.green.socket as socket
 import eventlet.green.ssl as ssl
 import eventlet
+import os
+import struct
+
+SO_PEERCRED = 17
 
 def sessionhdl(connection):
     #TODO: authenticate and authorize peer
     # For now, trying to test the console stuff, so let's just do n1.
     cfm = config.ConfigManager(tenant=0)
     consession = console.ConsoleSession(node='n1', configmanager=cfm,
-                                        datacallback=connection.write)
+                                        datacallback=connection.sendall)
     while (1):
-        data = connection.read()
+        data = connection.recv(4096)
         if not data:
             consession.destroy()
             return
         consession.write(data)
 
 
-def _handler():
+def _tlshandler():
     plainsocket = socket.socket()
     plainsocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     srv = ssl.wrap_socket(plainsocket, keyfile="/etc/confluent/privkey.pem",
@@ -38,6 +42,25 @@ def _handler():
         cnn, addr = srv.accept()
         eventlet.spawn_n(sessionhdl, cnn)
 
+
+def _unixdomainhandler():
+    unixsocket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    try:
+        os.remove("/var/run/confluent/api.sock")
+    except OSError:  # if file does not exist, no big deal
+        pass
+    unixsocket.bind("/var/run/confluent/api.sock")
+    unixsocket.listen(5)
+    while (1):
+        cnn, addr = unixsocket.accept()
+        creds = cnn.getsockopt(socket.SOL_SOCKET, SO_PEERCRED,
+                                struct.calcsize('3i'))
+        print struct.unpack('3i',creds)
+        eventlet.spawn_n(sessionhdl, cnn)
+
+
+
 class SockApi(object):
     def start(self):
-        self.server = eventlet.spawn(_handler)
+        self.tlsserver = eventlet.spawn(_tlshandler)
+        self.unixdomainserver = eventlet.spawn(_unixdomainhandler)
