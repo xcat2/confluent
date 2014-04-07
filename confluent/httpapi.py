92512da79fce185c54b6a3b486d91164c8244e74
Jarrod Johnson <jbjohnso@us.ibm.com>
2014-04-07 16:43:39 -0400

    Apply IBM 2014 copyright and Apache 2 license to project

--- confluent/httpapi.py
+++ confluent/httpapi.py
@@ -1,5 +1,18 @@
-# Copyright (C) IBM 2013
-# All rights reserved
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2014 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # This SCGI server provides a http wrap to confluent api
 # It additionally manages httprequest console sessions as supported by
 # shillinabox javascript
