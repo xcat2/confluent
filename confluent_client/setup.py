3443a7355888a9a4fbd6b9de8c8f84c88643e5af
Jarrod Johnson <jbjohnso@us.ibm.com>
2014-05-09 16:45:55 -0400

    Refresh package data to assure better pyghmi and confetty

--- confluent_client/setup.py
+++ confluent_client/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='confluent_client',
-    version='0.1.1',
+    version='0.1.2',
     author='Jarrod Johnson',
     author_email='jbjohnso@us.ibm.com',
     url='http://xcat.sf.net/',
     packages=['confluent'],
-    install_requires=['confluent_common>=0.1.1'],
+    install_requires=['confluent_common>=0.1.0'],
     scripts=['bin/confetty'],
 )
