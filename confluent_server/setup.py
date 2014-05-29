c938a280f9b976031635cc0e96371960640acdc5
Jarrod Johnson <jbjohnso@us.ibm.com>
2014-05-29 11:04:02 -0400

    Update version to reflect changes

--- confluent_server/setup.py
+++ confluent_server/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='confluent_server',
-    version='0.1.8',
+    version='0.1.9',
     author='Jarrod Johnson',
     author_email='jbjohnso@us.ibm.com',
     url='http://xcat.sf.net/',
