e08a79b0673f2ee5c4fd93d4ceb5ef996976d933
Jarrod Johnson <jjohnson2@lenovo.com>
2017-02-09 14:46:56 -0500

    Update copyright year

--- confluent_server/confluent/discovery/ssdp.py
+++ confluent_server/confluent/discovery/ssdp.py
@@ -1,6 +1,6 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright 2016 Lenovo
+# Copyright 2017 Lenovo
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
