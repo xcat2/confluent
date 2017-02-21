364a17f31ae00ca639800b08bee26a2968ae365b
Jarrod Johnson <jjohnson2@lenovo.com>
2017-02-21 16:58:07 -0500

    Implement IPv4 addr/netmask config apply
    Also start getting ready for user/password code tomorrow.

--- confluent_server/confluent/discovery/handlers/bmc.py
+++ confluent_server/confluent/discovery/handlers/bmc.py
@@ -13,8 +13,8 @@
 # limitations under the License.
 
 import confluent.discovery.handlers.generic as generic
-
-import eventlet.green.threading
+import confluent.exceptions as exc
+import confluent.netutil as netutil
 import eventlet.support.greendns
 
 # Provide foundation for general IPMI device configuration
@@ -63,8 +63,42 @@ class NodeHandler(generic.NodeHandler):
         ic = self._get_ipmicmd()
         currusers = ic.get_users()
         lanchan = ic.get_network_channel()
-        lockeduserdata = ic.xraw_command(netfn=6, command=0x44, data=(lanchan,
+        userdata = ic.xraw_command(netfn=6, command=0x44, data=(lanchan,
                                                                       1))
-        print(repr(lockeduserdata))
+        userdata = bytearray(userdata['data'])
+        maxusers = userdata[0] & 0b111111
+        enabledusers = userdata[1] & 0b111111
+        lockedusers = userdata[2] & 0b111111
+        cfg = self.configmanager
+        cd = cfg.get_node_attributes(
+            nodename, ['secret.hardwaremanagementuser',
+                       'secret.hardwaremanagementpassword',
+                       'hardwaremanagement.manager'], True)
+        cd = cd.get(nodename, {})
+        if ('secret.hardwaremanagementuser' not in cd or
+                'secret.hardwaremanagementpassword' not in cd):
+            raise exc.TargetEndpointBadCredentials(
+                'Missing user and/or password')
+        if ('hardwaremanagement.manager' in cd and
+                cd['hardwaremanagement.manager']['value'] and
+                not cd['hardwaremanagement.manager']['value'].startswith(
+                    'fe80::')):
+            newip = cd['hardwaremanagement.manager']['value']
+            if ':' in newip:
+                raise exc.NotImplementedException('IPv6 remote config TODO')
+            plen = netutil.get_prefix_len_for_ip(newip)
+            newip = '{0}/{1}'.format(newip, plen)
+            ic.set_net_configuration(ipv4_address=newip,
+                                     ipv4_configuration='static')
+        elif self.ipaddr.startswith('fe80::'):
+            cfg.set_node_attributes(
+                {nodename: {'hardwaremanagement.manager': self.ipaddr}})
+        else:
+            raise exc.TargetEndpointUnreachable(
+                'hardwaremanagement.manager must be set to desired address')
+        print(repr(cd))
+        print(maxusers)
+        print(enabledusers)
+        print(lockedusers)
         print(repr(currusers))
         return
