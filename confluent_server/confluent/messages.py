c6516f9d62d681891395c5e0489690b73882c37c
Jarrod Johnson <jjohnson2@lenovo.com>
2019-04-22 12:54:49 -0400

    Support redfish event logs

--- confluent_server/confluent/messages.py
+++ confluent_server/confluent/messages.py
@@ -1192,6 +1192,7 @@ class EventCollection(ConfluentMessage):
                 'event': event.get('event', None),
                 'severity': event['severity'],
                 'timestamp': event.get('timestamp', None),
+                'message': event.get('message', None),
                 'record_id': event.get('record_id', None),
             }
             if event['severity'] not in valid_health_values:
