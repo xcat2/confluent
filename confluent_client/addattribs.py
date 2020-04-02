b3c878462f42a5639d2b6b9340e9db4465653720
Jarrod Johnson <jjohnson2@lenovo.com>
2020-04-02 11:00:45 -0400

    Integrate attributes into man pages

---
+++ confluent_client/addattribs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import os
+import sys
+path = os.path.dirname(os.path.realpath(__file__))
+path = os.path.realpath(os.path.join(path, '..', 'confluent_server'))
+print(repr(path))
+sys.path.append(path)
+
+import confluent.config.attributes as attr
+import shutil
+
+shutil.copyfile('doc/man/nodeattrib.ronn.tmpl', 'doc/man/nodeattrib.ronn')
+shutil.copyfile('doc/man/nodegroupattrib.ronn.tmpl', 'doc/man/nodegroupattrib.ronn')
+with open('doc/man/nodeattrib.ronn', 'a') as outf:
+    for field in sorted(attr.node):
+        outf.write('\n* `{0}`:\n  {1}\n'.format(field, attr.node[field]['description']))
+with open('doc/man/nodegroupattrib.ronn', 'a') as outf:
+    for field in sorted(attr.node):
+        outf.write('\n* `{0}`:\n  {1}\n'.format(field, attr.node[field]['description']))
+
+
