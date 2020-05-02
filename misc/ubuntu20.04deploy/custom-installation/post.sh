00eb72a6274636c4cceaac338277b1b4763c948d
Jarrod Johnson <jjohnson2@lenovo.com>
2020-05-01 22:17:27 -0400

    Have ssh_known_hosts ready post install for ubuntu

--- misc/ubuntu20.04deploy/custom-installation/post.sh
+++ misc/ubuntu20.04deploy/custom-installation/post.sh
@@ -1,6 +1,10 @@
 cp -a /root/.ssh /target/root/
 mkdir -p /target/etc/confluent/ssh/sshd_config.d/
 cp /custom-installation/confluent/* /target/etc/confluent/
+for i in /custom-installation/ssh/*.ca; do
+    echo '@cert-authority *' $(cat $i) >> /target/etc/ssh/ssh_known_hosts
+done
+
 cp -a /etc/ssh/ssh_host* /target/etc/confluent/ssh/
 cp -a /etc/ssh/sshd_config.d/confluent.conf /target/etc/confluent/ssh/sshd_config.d/
 cp /custom-installation/firstboot.sh /target/etc/confluent/firstboot.sh
