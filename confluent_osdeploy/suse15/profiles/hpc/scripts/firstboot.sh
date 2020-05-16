a05bfcec80cd5b188d1f3281f088d6333f90a531
Jarrod Johnson <jjohnson2@lenovo.com>
2020-05-15 20:13:06 -0400

    Fix suse15 firstboot
    It was not using correct CA path nor was it filling in
    authentication headers.

--- confluent_osdeploy/suse15/profiles/hpc/scripts/firstboot.sh
+++ confluent_osdeploy/suse15/profiles/hpc/scripts/firstboot.sh
@@ -7,4 +7,4 @@ mgr=$(grep ^deploy_server /etc/confluent/confluent.deploycfg|awk '{print $2}')
 profile=$(grep ^profile: /etc/confluent/confluent.deploycfg|sed -e 's/^rootpassword: //')
 proto=$(grep ^protocol: /etc/confluent/confluent.deploycfg |awk '{print $2}')
 apikey=$(cat /etc/confluent/confluent.apikey)
-curl -f -X POST -d "status: complete" https://$mgr/confluent-api/self/updatestatus
+curl --capath /etc/confluent/tls -H "CONFLUENT_NODENAME: $nodename" -H "CONFLUENT_APIKEY: $apikey" -f -X POST -d "status: complete" https://$mgr/confluent-api/self/updatestatus
