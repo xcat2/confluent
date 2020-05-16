2211755bf701f74470053bc3a24a5368454986fc
Jarrod Johnson <jjohnson2@lenovo.com>
2020-05-16 10:14:23 -0400

    Have SUSE profile work for LEAP and SLE
    Add a directive to comment out the superfluous autoyast
    content from SLE when dealing with the more
    straightforward LEAP.

--- confluent_osdeploy/suse15/profiles/hpc/initprofile.sh
+++ confluent_osdeploy/suse15/profiles/hpc/initprofile.sh
@@ -12,3 +12,9 @@ ln -s $1/boot/x86_64/loader/linux $2/boot/kernel && \
 ln -s $1/boot/x86_64/loader/initrd $2/boot/initramfs/distribution && \
 mkdir -p $2/boot/efi/boot && \
 ln -s $1/EFI/BOOT/bootx64.efi $1/EFI/BOOT/grub.efi $2/boot/efi/boot/
+profile=$(basename $2)
+if [[ $profile =~ ^sle.* ]]; then
+	sed -i 's/%%IFSLE%%//;s/%%ENDIFSLE%%//' $2/autoyast
+else
+	sed -i 's/%%IFSLE%%/<!--/;s/%%ENDIFSLE%%/-->/' $2/autoyast
+fi
