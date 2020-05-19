92f8acab7a957c045fe589ea259d9b621bd86d75
Jarrod Johnson <jjohnson2@lenovo.com>
2020-05-18 22:07:03 -0400

    Fix target path of tls anchors

--- confluent_osdeploy/suse15/profiles/hpc/scripts/post.sh
+++ confluent_osdeploy/suse15/profiles/hpc/scripts/post.sh
@@ -44,5 +44,5 @@ cp /tmp/confluent.* /mnt/etc/confluent/
 cp -a /tls /mnt/etc/confluent/
 cp -a /tls/* /mnt/var/lib/ca-certificates/openssl
 cp -a /tls/* /mnt/etc/ssl/certs
-cp -a /tls/*.cert /etc/pki/trust/anchors
+cp -a /tls/*.cert /mnt/etc/pki/trust/anchors
 
