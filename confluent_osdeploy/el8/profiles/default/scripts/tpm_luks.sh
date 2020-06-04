0016c31fef742e4f8859a80f6b8f324744508929
Jarrod Johnson <jjohnson2@lenovo.com>
2020-06-04 13:41:00 -0400

    Fix encrypted boot on 8.1
    8.1 requires -f or it will fail.

--- confluent_osdeploy/el8/profiles/default/scripts/tpm_luks.sh
+++ confluent_osdeploy/el8/profiles/default/scripts/tpm_luks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cryptdisk=$(blkid -t TYPE="crypto_LUKS"|sed -e s/:.*//)
-clevis luks bind -d $cryptdisk -k - tpm2 '{"pcr_bank": "sha256", "pcr_ids": "7"}' < /etc/confluent/confluent.apikey
+clevis luks bind -f -d $cryptdisk -k - tpm2 '{"pcr_bank": "sha256", "pcr_ids": "7"}' < /etc/confluent/confluent.apikey
 cryptsetup luksRemoveKey $cryptdisk < /etc/confluent/confluent.apikey
