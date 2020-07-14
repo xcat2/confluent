dad68bdf63ab5d1643853534dbb42cf48e3eab92
Jarrod Johnson <jjohnson2@lenovo.com>
2020-07-14 14:40:05 -0400

    Conditionally include clevis-dracut
    If not using LUKS, don't bother with clevis-dracut.
    
    In 8.1, clevis-dracut will cause problems with network
    in initrd time.

--- confluent_osdeploy/el8/profiles/default/scripts/pre.sh
+++ confluent_osdeploy/el8/profiles/default/scripts/pre.sh
@@ -46,9 +46,11 @@ if [ -f "/run/install/cmdline.d/01-autocons.conf" ]; then
 fi
 cryptboot=$(grep ^encryptboot: /etc/confluent/confluent.deploycfg | awk '{print $2}')
 LUKSPARTY=''
+touch /tmp/cryptpkglist
 if [ "$cryptboot" == "tpm2" ]; then
 	LUKSPARTY="--encrypted --passphrase=$(cat /etc/confluent/confluent.apikey)"
 	echo $cryptboot >> /tmp/cryptboot
+	echo clevis-dracut >> /tmp/cryptpkglist
 fi
 
 
