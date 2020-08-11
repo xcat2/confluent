644433fa5dca1a981e4bc6c82ebc1e4fea739a81
Jarrod Johnson <jjohnson2@lenovo.com>
2020-08-11 09:54:53 -0400

    Refactor genesis to have addons.cpio
    This makes an easier division between pure CentOS
    contributions and 'genesis-scripts' type output.
    Do so in a manner consistent with how 'real'
    operating systems get this added.

---
+++ confluent_osdeploy/genesis/initramfs/usr/lib/dracut/hooks/cmdline/10-genesis.sh
@@ -0,0 +1,13 @@
+root=1
+rootok=1
+netroot=genesis
+clear
+echo PS1="'"'[genesis running on \H \w]$ '"'" >> ~/.bashrc
+echo PS1="'"'[genesis running on \H \w]$ '"'" >> ~/.bash_profile
+mkdir -p /etc/ssh
+mkdir -p /var/tmp/
+mkdir -p /var/empty/sshd
+sed -i '/^root:x/d' /etc/passwd
+echo root:x:0:0::/:/bin/bash >> /etc/passwd
+echo sshd:x:30:30:SSH User:/var/empty/sshd:/sbin/nologin >> /etc/passwd
+tmux -L console new-sessiod /bin/rungenesis
--- genesis/buildgenesis.sh
+++ genesis/buildgenesis.sh
@@ -8,5 +8,11 @@ dracut --xz -N -m "genesis base" -f boot/initramfs/distribution $(uname -r)
 cp -f /boot/vmlinuz-$(uname -r) boot/kernel
 cp /boot/efi/EFI/BOOT/BOOTX64.EFI boot/efi/boot
 cp /boot/efi/EFI/centos/grubx64.efi boot/efi/boot/grubx64.efi
+tar cf ~/rpmbuild/SOURCES/confluent-genesis.tar boot
+rpmbuild -bb confluent-genesis.spec
 rm -rf /usr/lib/dracut/modules.d/97genesis
 cd -
+# getting src rpms would be nice, but centos isn't consistent..
+# skipcpio | xzcat | cpio -dumiv
+# dnf download --source $(rpm -qf $(find . -type f | sed -e 's/^.//') |sort -u|grep -v 'not owned')
+
