43ce1b17a7ecf195123819e80d45ee3fc4764fc4
Jarrod Johnson <jjohnson2@lenovo.com>
2020-08-11 18:11:48 -0400

    Rework genesis with more output and an onboot.sh

---
+++ confluent_osdeploy/genesis/profiles/default/scripts/onboot.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+. /tmp/functions
+# This runs whenever this genesis profile boots for customization
+# purposes
+
+# run_remote and run_remote_python are available to download scripts and
+# execute them.
+
