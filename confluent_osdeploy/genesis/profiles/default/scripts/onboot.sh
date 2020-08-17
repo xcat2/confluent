5bdd59bc673b19c3a70a2787392b495b18bfea56
Jarrod Johnson <jjohnson2@lenovo.com>
2020-08-17 14:23:21 -0400

    Add configbmc to el8 and genesis script examples

--- confluent_osdeploy/genesis/profiles/default/scripts/onboot.sh
+++ confluent_osdeploy/genesis/profiles/default/scripts/onboot.sh
@@ -6,3 +6,6 @@
 # run_remote and run_remote_python are available to download scripts and
 # execute them.
 
+# This is an example to request the BMC be configured on the network
+# according to how confluent has things configured:
+# run_remote_python configbmc -c
