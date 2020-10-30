71cc0adadd857e4a769c5acbbd8f18c78672d836
Jarrod Johnson <jjohnson2@lenovo.com>
2020-10-30 12:34:42 -0400

    Bump genesis to build more packages
    Also notate src.rpm retrieval procedure

--- genesis/buildgenesis.sh
+++ genesis/buildgenesis.sh
@@ -13,11 +13,15 @@ rpmbuild -bb confluent-genesis.spec
 rm -rf /usr/lib/dracut/modules.d/97genesis
 cd -
 # getting src rpms would be nice, but centos isn't consistent..
-# skipcpio | xzcat | cpio -dumiv
-# rpm -qf $(find . -type f | sed -e 's/^.//') |sort -u|grep -v 'not owned' > rpmlist
+# /usr/lib/dracut/skipcpio /opt/confluent/genesis/x86_64/boot/initramfs/distribution | xzcat | cpio -dumiv
+# rpm -qf $(find . -type f | sed -e 's/^.//') |sort -u|grep -v 'not owned' > ../rpmlist
+# for f in $(find . -type f | sed -e 's/^.//'); do echo -n $f:; rpm -qf $f ; done > ../annotedrprmlist
 # for i in $(cat rpmlist); do rpm -qi $i|grep Source; done |awk '{print $4}'|sort -u > srcrpmlist
-# for i in $(cat ../srcrpmlist); do wget http://vault.centos.org/8.2.2004/BaseOS/Source/SPackages/$i; done
-# http://vault.centos.org/8.2.2004/AppStream/Source/SPackages/$i
+# for i in $(cat ../srcrpmlist); do wget --continue http://vault.centos.org/8.2.2004/BaseOS/Source/SPackages/$i; done
+# ls > downloadedsrcpmlist
+# diff -u srcpmlist downloadedsrcrpmlist
+# diff -u srcrpmlist downloadedsrcpmrlist |grep ^-|grep -v srcrpmlist
+# for i in $(diff -u srcrpmlist downloadedsrcpmrlist |grep ^-|grep -v srcrpmlist|sed -e s/-//); do wget --continue http://vault.centos.org/8.2.2004/AppStream/Source/SPackages/$i; done
 
 
 
--- genesis/confluent-genesis.spec
+++ genesis/confluent-genesis.spec
@@ -1,5 +1,5 @@
 %define arch x86_64
-Version: 3.0.0
+Version: 3.1.0
 Release:  1
 Name: confluent-genesis-%{arch}
 BuildArch: noarch
