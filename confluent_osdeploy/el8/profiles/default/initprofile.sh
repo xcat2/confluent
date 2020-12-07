fa823510b6229c981dc2c8640f90bad40296046f
Jarrod Johnson <jjohnson2@lenovo.com>
2020-12-07 15:09:30 -0500

    Pretty-ify Oracle Linux name in profile.yaml
    Give it a nicer looking automatic label when
    Oracle Linux is detected.

--- confluent_osdeploy/el8/profiles/default/initprofile.sh
+++ confluent_osdeploy/el8/profiles/default/initprofile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sed -i 's/centos/CentOS/; s/rhel/Red Hat Enterprise Linux/' $2/profile.yaml
+sed -i 's/centos/CentOS/; s/rhel/Red Hat Enterprise Linux/; s/oraclelinux/Oracle Linux/' $2/profile.yaml
 ln -s $1/images/pxeboot/vmlinuz $2/boot/kernel && \
 ln -s $1/images/pxeboot/initrd.img $2/boot/initramfs/distribution
 mkdir -p $2/boot/efi/boot && \
