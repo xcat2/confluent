f32619aa3ed9f78f677dcaf29266a172b08e060d
Jarrod Johnson <jjohnson2@lenovo.com>
2021-04-29 09:20:24 -0400

    Allow firstboot to reboot in RH postscripts
    Sometimes firstboot scripts want to reboot.
    
    In such a case, start firstboot once, and if reboot prevents completion,
    then next boot will updatestatus, but not run firstboot scripts and
    deregister itself.

--- confluent_osdeploy/el7/profiles/default/scripts/firstboot.sh
+++ confluent_osdeploy/el7/profiles/default/scripts/firstboot.sh
@@ -9,19 +9,23 @@ nodename=$(grep ^NODENAME /etc/confluent/confluent.info|awk '{print $2}')
 apikey=$(cat /etc/confluent/confluent.apikey)
 mgr=$(grep deploy_server /etc/confluent/confluent.deploycfg|awk '{print $2}')
 profile=$(grep ^profile: /etc/confluent/confluent.deploycfg|awk '{print $2}')
-cat /etc/confluent/tls/*.pem >> /etc/pki/tls/certs/ca-bundle.crt
 export nodename mgr profile
 . /etc/confluent/functions
 
+if [ ! -f /etc/confluent/firstboot.ran ]; then
+    touch /etc/confluent/firstboot.ran
 
-run_remote firstboot.custom
-# Firstboot scripts may be placed into firstboot.d, e.g. firstboot.d/01-firstaction.sh, firstboot.d/02-secondaction.sh
-run_remote_parts firstboot
+    cat /etc/confluent/tls/*.pem >> /etc/pki/tls/certs/ca-bundle.crt
 
+    run_remote firstboot.custom
+    # Firstboot scripts may be placed into firstboot.d, e.g. firstboot.d/01-firstaction.sh, firstboot.d/02-secondaction.sh
+    run_remote_parts firstboot
 
-# Induce execution of remote configuration, e.g. ansible plays in ansible/firstboot.d/
-run_remote_config firstboot
+    # Induce execution of remote configuration, e.g. ansible plays in ansible/firstboot.d/
+    run_remote_config firstboot
+fi
 
 curl -X POST -d 'status: complete' -H "CONFLUENT_NODENAME: $nodename" -H "CONFLUENT_APIKEY: $apikey" https://$mgr/confluent-api/self/updatestatus
 systemctl disable firstboot
 rm /etc/systemd/system/firstboot.service
+rm /etc/confluent/firstboot.ran
