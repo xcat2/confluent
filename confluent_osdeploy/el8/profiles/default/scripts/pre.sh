d899cfbf1a2a2edce9cee7225c0a15aeb895a28e
Jarrod Johnson <jjohnson2@lenovo.com>
2021-09-29 14:31:18 -0400

    Clean disk to install in pre
    Installers frequently fail if there's a derelict lvm on disk.
    
    Explicitly remove the on-disk content and deactive lvm to let
    anaconda work reliably.

--- confluent_osdeploy/el8/profiles/default/scripts/pre.sh
+++ confluent_osdeploy/el8/profiles/default/scripts/pre.sh
@@ -93,5 +93,7 @@ if [ -e /tmp/installdisk -a ! -e /tmp/partitioning ]; then
     echo clearpart --all --initlabel >> /tmp/partitioning
     echo ignoredisk --only-use $(cat /tmp/installdisk) >> /tmp/partitioning
     echo autopart --nohome $LUKSPARTY >> /tmp/partitioning
+     dd if=/dev/zero of=/dev/$(cat /tmp/installdisk) bs=1M count=1
+     vgchange -a n
 fi
 kill $logshowpid
