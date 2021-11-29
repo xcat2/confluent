00e853130f1a9f5442383109e1e2d1d147f23103
Jarrod Johnson <jjohnson2@lenovo.com>
2021-11-29 16:03:06 -0500

    Fallback to other location for grub

--- confluent_osdeploy/coreos/profiles/default/initprofile.sh
+++ confluent_osdeploy/coreos/profiles/default/initprofile.sh
@@ -4,5 +4,6 @@ ln -s $1/images/pxeboot/initrd.img $2/boot/initramfs/distribution && \
 ln -s $1/images/ignition.img $2/boot/initramfs/ignition.img && \
 mkdir -p $2/boot/efi/boot/ && \
 ln -s $1/images/pxeboot/rootfs.img $2/ && \
-mcopy -i $1/images/efiboot.img ::efi/redhat/grubx64.efi $2/boot/efi/boot/ && \
+(mcopy -i $1/images/efiboot.img ::efi/redhat/grubx64.efi $2/boot/efi/boot/ || \
+mcopy -i $1/images/efiboot.img ::efi/boot/grubx64.efi $2/boot/efi/boot/) && \
 mcopy -i $1/images/efiboot.img ::efi/boot/bootx64.efi $2/boot/efi/boot/
