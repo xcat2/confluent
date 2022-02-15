3f53cb939a1ef1faa85ce3afc171a1bb2b254110
Jarrod Johnson <jjohnson2@lenovo.com>
2022-02-15 10:32:39 -0500

    Add mkdir -p to build script for genesis

--- genesis/buildgenesis.sh
+++ genesis/buildgenesis.sh
@@ -8,6 +8,7 @@ dracut --no-early-microcode --xz -N -m "genesis base" -f boot/initramfs/distribu
 cp -f /boot/vmlinuz-$(uname -r) boot/kernel
 cp /boot/efi/EFI/BOOT/BOOTX64.EFI boot/efi/boot
 cp /boot/efi/EFI/centos/grubx64.efi boot/efi/boot/grubx64.efi
+mkdir -p ~/rpmbuild/SOURCES/
 tar cf ~/rpmbuild/SOURCES/confluent-genesis.tar boot
 rpmbuild -bb confluent-genesis.spec
 rm -rf /usr/lib/dracut/modules.d/97genesis
