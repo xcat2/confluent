5fb766e62b483e54b70d65f26112dbcc0ee8939b
Jarrod Johnson <jjohnson2@lenovo.com>
2022-03-03 11:11:29 -0500

    Move apiclient consistently to /opt/confluent/bin
    It's more reasonable to have
    it in a bin directory

--- confluent_osdeploy/suse15/profiles/hpc/scripts/prechroot.sh
+++ confluent_osdeploy/suse15/profiles/hpc/scripts/prechroot.sh
@@ -18,7 +18,8 @@ cp -a /tls/* /mnt/var/lib/ca-certificates/openssl
 cp -a /tls/* /mnt/var/lib/ca-certificates/pem
 cp -a /tls/*.pem /mnt/etc/pki/trust/anchors
 cat /tls/*.pem > /mnt/etc/confluent/ca.pem
-cp /opt/confluent/bin/apiclient /mnt/etc/confluent
+mkdir -p /mnt/opt/confluent/bin
+cp /opt/confluent/bin/apiclient /opt/confluent/bin/
 
 run_remote setupssh.sh
 
