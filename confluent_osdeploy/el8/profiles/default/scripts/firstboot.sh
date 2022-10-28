fd14221ab50f4fc476a0ababdfbf59bb8c7c8238
Jarrod Johnson <jjohnson2@lenovo.com>
2022-10-28 09:30:12 -0400

    Avoid truncating console logging of firstboot
    With significant firstboot output, there was a tendency
    for tail to be killed before it relayed all the content.
    
    Change to run the firstboot in a subshell in the background,
    and have tail explicitly run until that subshell naturally
    exits and then tail will cleanly exit

--- confluent_osdeploy/el8/profiles/default/scripts/firstboot.sh
+++ confluent_osdeploy/el8/profiles/default/scripts/firstboot.sh
@@ -23,11 +23,10 @@ fi
 confluent_profile=$(grep ^profile: /etc/confluent/confluent.deploycfg|awk '{print $2}')
 export nodename confluent_mgr confluent_profile
 . /etc/confluent/functions
+(
 exec >> /var/log/confluent/confluent-firstboot.log
 exec 2>> /var/log/confluent/confluent-firstboot.log
 chmod 600 /var/log/confluent/confluent-firstboot.log
-tail -n 0 -f /var/log/confluent/confluent-firstboot.log > /dev/console &
-logshowpid=$!
 while ! ping -c 1 $confluent_pingtarget >& /dev/null; do
 	sleep 1
 done
@@ -50,4 +49,5 @@ curl -X POST -d 'status: complete' -H "CONFLUENT_NODENAME: $nodename" -H "CONFLU
 systemctl disable firstboot
 rm /etc/systemd/system/firstboot.service
 rm /etc/confluent/firstboot.ran
-kill $logshowpid
+) &
+tail --pid $! -n 0 -F /var/log/confluent/confluent-firstboot.log > /dev/console
