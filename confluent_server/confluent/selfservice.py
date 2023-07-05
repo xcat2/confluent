d0f978548eb0fac2cfcb78ca054248ea15774a34
Simon Thompson <sthompson2@lenovo.com>
2023-07-05 21:23:55 +0200

    sort directory listing to make ansible plays and script order deterministic

--- confluent_server/confluent/selfservice.py
+++ confluent_server/confluent/selfservice.py
@@ -570,7 +570,7 @@ def get_scriptlist(scriptcat, cfg, nodename, pathtemplate):
     if not os.path.isdir(target) and os.path.isdir(target + '.d'):
         target = target + '.d'
     try:
-        slist = os.listdir(target)
+        slist = sorted(os.listdir(target))
     except OSError:
         pass
     return slist, profile
