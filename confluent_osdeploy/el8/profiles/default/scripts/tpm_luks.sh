e6dc383d2598b5c6a9d851b9ed7b5894a25e0532
Jarrod Johnson <jjohnson2@lenovo.com>
2024-07-29 11:22:07 -0400

    Fix mistake in EL8/EL9 LUKS

--- confluent_osdeploy/el8/profiles/default/scripts/tpm_luks.sh
+++ confluent_osdeploy/el8/profiles/default/scripts/tpm_luks.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cryptdisk=$(blkid -t TYPE="crypto_LUKS"|sed -e s/:.*//)
-clevis luks bind -f -d $cryptdisk -k /etc/cofluent/luks.key tpm2 '{}'
+clevis luks bind -f -d $cryptdisk -k - tpm2 '{}' < /etc/confluent/luks.key
 chmod 000 /etc/confluent/luks.key
 #cryptsetup luksRemoveKey $cryptdisk < /etc/confluent/confluent.apikey
