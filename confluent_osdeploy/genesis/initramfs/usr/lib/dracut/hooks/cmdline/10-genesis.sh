02ec40092e16523a3bb5c8475e4f45dd71c02ee7
Jarrod Johnson <jjohnson2@lenovo.com>
2025-03-04 15:12:59 -0500

    Have genesis optionally GUI capable
    Have Genesis work with newer distribution base, also support seatd/sway when
    genesis contains it.

--- confluent_osdeploy/genesis/initramfs/usr/lib/dracut/hooks/cmdline/10-genesis.sh
+++ confluent_osdeploy/genesis/initramfs/usr/lib/dracut/hooks/cmdline/10-genesis.sh
@@ -7,7 +7,7 @@ echo PS1="'"'[genesis running on \H \w]$ '"'" >> ~/.bash_profile
 mkdir -p /etc/ssh
 mkdir -p /var/tmp/
 mkdir -p /var/empty/sshd
-sed -i '/^root:x/d' /etc/passwd
+sed -i '/^root:/d' /etc/passwd
 echo root:x:0:0::/:/bin/bash >> /etc/passwd
 echo sshd:x:30:30:SSH User:/var/empty/sshd:/sbin/nologin >> /etc/passwd
 tmux new-session -d sh /opt/confluent/bin/rungenesis
