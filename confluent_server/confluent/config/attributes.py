dbda4f45a1c49ec2cbbd357e99a59620743eef70
Jarrod Johnson <jjohnson2@lenovo.com>
2025-08-12 10:49:01 -0400

    Add 'proxmox' to valid values for console options

--- confluent_server/confluent/config/attributes.py
+++ confluent_server/confluent/config/attributes.py
@@ -376,7 +376,7 @@ node = {
                         'the managed node.  If not specified, then console '
                         'is disabled.  "ipmi" should be specified for most '
                         'systems if console is desired.'),
-        'validvalues': ('ssh', 'ipmi', 'openbmc', 'tsmsol', 'vcenter'),
+        'validvalues': ('ssh', 'ipmi', 'openbmc', 'tsmsol', 'vcenter', 'proxmox'),
     },
 #    'virtualization.host': {
 #        'description': ('Hypervisor where this node does/should reside'),
