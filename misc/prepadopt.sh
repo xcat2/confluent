4f75d4942bc012982e1be67578d38b9f2881ba51
Jarrod Johnson <jjohnson2@lenovo.com>
2025-11-20 16:05:22 -0500

    Modify adoption process:
    Restore useinsecureprotocols if set directly on node
    
    Switch from pxe-style to identity-file based node api token for hardened node authentication

--- misc/prepadopt.sh
+++ misc/prepadopt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TARGNODE=$1
 TARGPROF=$2
+TARGIDENT=$3
 TMPDIR=$(mktemp -d)
 cd $TMPDIR
 DEPLOYSRV=$(echo $SSH_CLIENT|awk '{print $1}')
@@ -16,10 +17,12 @@ cpio -dumi < addons.cpio
 systemctl status firewalld >& /dev/null && FWACTIVE=1
 if [ "$FWACTIVE" == 1 ]; then systemctl stop firewalld; fi
 opt/confluent/bin/copernicus  > /etc/confluent/confluent.info
-opt/confluent/bin/clortho $TARGNODE $DEPLOYSRV > /etc/confluent/confluent.apikey
+#opt/confluent/bin/clortho $TARGNODE $DEPLOYSRV > /etc/confluent/confluent.apikey
+. /etc/confluent/functions
+confluentpython opt/confluent/bin/apiclient -i $TAGRIDENT /confluent-api/self/deploycfg2 > /etc/confluent/confluent.deploycfg
 if [ "$FWACTIVE" == 1 ]; then systemctl start firewalld; fi
 cp opt/confluent/bin/apiclient /opt/confluent/bin
-curl -sg -H "CONFLUENT_APIKEY: $(cat /etc/confluent/confluent.apikey)" -H "CONFLUENT_NODENAME: $TARGNODE" https://$UDEPLOYSRV/confluent-api/self/deploycfg2 > /etc/confluent/confluent.deploycfg
+#curl -sg -H "CONFLUENT_APIKEY: $(cat /etc/confluent/confluent.apikey)" -H "CONFLUENT_NODENAME: $TARGNODE" https://$UDEPLOYSRV/confluent-api/self/deploycfg2 > /etc/confluent/confluent.deploycfg
 # python3 /opt/confluent/bin/apiclient /confluent-api/self/deploycfg2 > /etc/confluent/confluent.deploycfg
 cd -
 echo rm -rf $TMPDIR
