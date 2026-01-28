291363c5820cddfa606c7f1a1bb0742e0d2e5b9d
Jarrod Johnson <jjohnson2@lenovo.com>
2026-01-28 14:49:58 -0500

    Update some code for async

--- confluent_server/confluent/plugins/hardwaremanagement/ipmi.py
+++ confluent_server/confluent/plugins/hardwaremanagement/ipmi.py
@@ -587,7 +587,7 @@ class IpmiHandler:
         elif self.element[:3] == ['inventory', 'firmware', 'updatestatus']:
             self.handle_update_status()
         elif self.element[0] == 'inventory':
-            self.handle_inventory()
+            await self.handle_inventory()
         elif self.element == ['media', 'attach']:
             self.handle_attach_media()
         elif self.element == ['media', 'detach']:
@@ -1005,7 +1005,7 @@ class IpmiHandler:
             status = self.ipmicmd.get_update_status()
             self.output.put(msg.KeyValueData({'status': status}, self.node))
 
-    def handle_inventory(self):
+    async def handle_inventory(self):
         if self.element[1] == 'firmware':
             if len(self.element) == 3:
                 return self.list_firmware()
@@ -1015,7 +1015,7 @@ class IpmiHandler:
             if len(self.element) == 3:  # list things in inventory
                 return self.list_inventory()
             elif len(self.element) == 4:  # actually read inventory data
-                return self.read_inventory(self.element[-1])
+                return await self.read_inventory(self.element[-1])
         raise Exception('Unsupported scenario...')
 
     def list_leds(self):
