23556f469872e0602ae7dc7065050bbafff7134f
Jarrod Johnson <jjohnson2@lenovo.com>
2026-09-11 19:34:21 -0400

    Fix potential uninitialized variable in get_switchcreds

--- confluent_server/confluent/networking/switchutil.py
+++ confluent_server/confluent/networking/switchutil.py
@@ -40,6 +40,7 @@ def get_switchcreds(configmanager, switches):
         password = switchparms.get(
             'secret.snmpcommunity', {}).get('value', None)
         privacy_protocol = None
+        auth_protocol = None
         if not password:
             password = switchparms.get(
                 'secret.hardwaremanagementpassword', {}).get('value',
